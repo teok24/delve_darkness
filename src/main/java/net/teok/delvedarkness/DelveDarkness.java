8c33121a0ae6f47f578208a82ee8f9c6d5775a4e
teok24 <114262077+teok24@users.noreply.github.com>
2023-03-20 18:07:16 +1100

    Added Test Comment

--- src/main/java/net/teok/delvedarkness/DelveDarkness.java
+++ src/main/java/net/teok/delvedarkness/DelveDarkness.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 
 public class DelveDarkness implements ModInitializer {
 
+	//Test Commit
 	public static final String MOD_ID = "delvedarkness";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
