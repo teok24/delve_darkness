cae06fe8397f46f8323855fe2ecac3aeca8c61a8
teok24 <114262077+teok24@users.noreply.github.com>
2023-03-20 18:05:55 +1100

    Initial commit

---
+++ src/main/java/net/teok/delvedarkness/DelveDarkness.java
@@ -0,0 +1,16 @@
+package net.teok.delvedarkness;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DelveDarkness implements ModInitializer {
+
+	public static final String MOD_ID = "delvedarkness";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/teok/delvedarkness/DelveDarknessClient.java
@@ -0,0 +1,10 @@
+package net.teok.delvedarkness;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DelveDarknessClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
