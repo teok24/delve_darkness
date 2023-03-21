ee8d3be7dc12eddee691c4ac8cf2a048be82ff68
teok24 <114262077+teok24@users.noreply.github.com>
2023-03-22 01:31:18 +1100

    changed darkness stat from float to int, changed max darkness stat to 8 for 4 heart damage at max darkness

--- src/main/java/net/teok/delvedarkness/util/DarknessData.java
+++ src/main/java/net/teok/delvedarkness/util/DarknessData.java
@@ -3,26 +3,26 @@ package net.teok.delvedarkness.util;
 import net.minecraft.nbt.NbtCompound;
 
 public class DarknessData {
-    public static float addDarkness(IEntityDataSaver player, float amount)
+    public static float addDarkness(IEntityDataSaver player, int amount)
     {
         NbtCompound nbt = player.getPersistentData();
-        float darkness = nbt.getFloat("darkness");
-        if (darkness + amount >= 5)
+        int darkness = nbt.getInt("darkness");
+        if (darkness + amount >= 10)
         {
-            darkness = 5;
+            darkness = 8; //control by config
         }
         else
         {
             darkness += amount;
         }
-        nbt.putFloat("darkness", darkness);
+        nbt.putInt("darkness", darkness);
         //sync data
         return darkness;
     }
-    public static float removeDarkness(IEntityDataSaver player, float amount)
+    public static float removeDarkness(IEntityDataSaver player, int amount)
     {
         NbtCompound nbt = player.getPersistentData();
-        float darkness = nbt.getFloat("darkness");
+        int darkness = nbt.getInt("darkness");
         if (darkness - amount <= 0)
         {
             darkness = 0;
@@ -31,15 +31,15 @@ public class DarknessData {
         {
             darkness -= amount;
         }
-        nbt.putFloat("darkness", darkness);
+        nbt.putInt("darkness", darkness);
         //sync data
         return darkness;
     }
     public static void resetDarkness(IEntityDataSaver player)
     {
         NbtCompound nbt = player.getPersistentData();
-        float darkness = nbt.getFloat("darkness");
+        int darkness = nbt.getInt("darkness");
         darkness = 0;
-        nbt.putFloat("darkness", darkness);
+        nbt.putInt("darkness", darkness);
     }
 }
